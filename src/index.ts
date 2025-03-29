17f94abfb3d9dcf7745ce2078dfdcee652464fea
geeknik <466878+geeknik@users.noreply.github.com>
2025-03-29 17:27:36 -0500

    Add main index.ts entry point

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * TokenGuardian - Advanced token security, leak prevention and rotation automation
+ */
+
+export { TokenGuardian } from './TokenGuardian';
+export { ScanResult } from './interfaces/ScanResult';
+export { TokenConfig } from './interfaces/TokenConfig';
+export { GuardianConfig } from './interfaces/GuardianConfig';
+export { RotationResult } from './interfaces/RotationResult';
